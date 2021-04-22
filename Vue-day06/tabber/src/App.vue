189f31e8b3db3d64556c0d7e00908fe3d1740a9d
auue_glb <goodMorning_glb@qq.com>
2021-04-22 22:44:40 +0800

    tabber

---
+++ Vue-day06/tabber/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <main-tab-bar></main-tab-bar>
+  </div>
+</template>
+
+<script>
+import MainTabBar from "components/MainTabBar";
+export default {
+  name: "App",
+  components: {
+    MainTabBar,
+  },
+};
+</script>
+
+<style>
+/* 将样式文件导入到App.vue而不是index.js,使用@import方法导入 */
+@import "./assets/css/base.css";
+</style>
---
+++ Vue-day06/tabber/src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import Home from '../views/home/Home.vue'
+Vue.use(Router)
+
+const Home = () => import('../views/home/Home.vue')
+const Cart = () => import('../views/cart/Cart.vue')
+const Category = () => import('../views/category/Category.vue')
+const Profile = () => import('../views/profile/Profile.vue')
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+
+export default new Router({
+  routes,
+  mode: 'history'
+})
+
